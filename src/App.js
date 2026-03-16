4b4fcf070edabf3aa0f4c86907d46c1f510e7e28
fediaiandrii <lezukokitagava@gmail.com>
2026-03-16 18:31:40 +0200

    Clean up CRA and add starter template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialProjects = [
+  {
+    id: 118836,
+    name: "Art Hiking",
+    image: "https://i.pravatar.cc/48?u=118836",
+    format: "16:9",
+    type: "Vlog",
+    stages: {
+      idea: true,
+      shoot: true,
+      edit: true,
+      color: true,
+      published: false,
+    },
+  },
+  {
+    id: 933372,
+    name: "Selfcare sport",
+    image: "https://i.pravatar.cc/48?u=933372",
+    format: "4:3",
+    type: "Animation",
+    stages: {
+      idea: true,
+      shoot: false,
+      edit: false,
+      color: false,
+      published: false,
+    },
+  },
+  {
+    id: 499476,
+    name: "Life design",
+    image: "https://i.pravatar.cc/48?u=499476",
+    format: "4:5",
+    type: "Timelaps",
+    stages: {
+      idea: true,
+      shoot: true,
+      edit: false,
+      color: false,
+      published: false,
+    },
+  },
+];
+
+export default function App() {
+  const [projects, setProjects] = useState(initialProjects);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar"></div>
     </div>
   );
 }
-
-export default App;
