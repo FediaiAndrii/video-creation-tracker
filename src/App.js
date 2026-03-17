062b2522c17db7b71de66e09e31f23a33fcfc376
fediaiandrii <lezukokitagava@gmail.com>
2026-03-17 14:29:13 +0200

    implement project selection and button toggle state

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ const initialProjects = [
       idea: true,
       shoot: true,
       edit: true,
-      color: true,
-      published: false,
+      publish: false,
     },
   },
   {
@@ -20,13 +19,12 @@ const initialProjects = [
     name: "Selfcare sport",
     image: "https://i.pravatar.cc/48?u=933372",
     format: "4:3",
-    type: "Animation",
+    type: "Timelaps",
     stages: {
       idea: true,
       shoot: false,
       edit: false,
-      color: false,
-      published: false,
+      publish: false,
     },
   },
   {
@@ -34,23 +32,85 @@ const initialProjects = [
     name: "Life design",
     image: "https://i.pravatar.cc/48?u=499476",
     format: "4:5",
-    type: "Timelaps",
+    type: "Vlog",
     stages: {
       idea: true,
       shoot: true,
       edit: false,
-      color: false,
-      published: false,
+      publish: false,
     },
   },
 ];
 
+const stageLabels = {
+  idea: "Planning",
+  shoot: "Shooting",
+  edit: "Editing",
+  publish: "Publishing",
+};
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
   const [projects, setProjects] = useState(initialProjects);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  function handleSelection(project) {
+    setSelectedProject(cur => (cur?.id === project.id ? null : project));
+  }
 
   return (
     <div className="app">
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <ProjectList
+          projects={projects}
+          selectedProject={selectedProject}
+          onSelection={handleSelection}
+        />
+      </div>
     </div>
+  );
+}
+
+function ProjectList({ projects, selectedProject, onSelection }) {
+  return (
+    <ul>
+      {projects.map(project => (
+        <Project
+          project={project}
+          key={project.id}
+          selectedProject={selectedProject}
+          onSelection={onSelection}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Project({ project, selectedProject, onSelection }) {
+  const isSelected = selectedProject?.id === project.id;
+  const activeStageEntry = Object.entries(project.stages).find(
+    ([key, status]) => status === false,
+  );
+
+  const currentStage = activeStageEntry
+    ? stageLabels[activeStageEntry[0]]
+    : "Done";
+
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={project.image} alt={project.name}></img>
+      <h3>{project.name}</h3>
+      <p>{currentStage}</p>
+      <Button onClick={() => onSelection(project)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
+    </li>
   );
 }
